fd650cf3db7cc43b474eac536d56421e79dc7325
Andriambololomanana Brondone <andriambololomananabrondone@gmail.com>
2024-01-17 18:10:08 +0300

    init project

---
+++ src/config/firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZI-pIzTXZ9dqUidSdZyq9vrGOBuL6GGY",
+  authDomain: "projshare-1.firebaseapp.com",
+  projectId: "projshare-1",
+  storageBucket: "projshare-1.appspot.com",
+  messagingSenderId: "663893838132",
+  appId: "1:663893838132:web:5d576deb31c937848d3d61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
